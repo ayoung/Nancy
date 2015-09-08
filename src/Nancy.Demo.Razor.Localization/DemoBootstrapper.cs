5a318b580522c6dec2a715e60c1d07954cfb9527
Andrew Young <andrew.young@carefusion.com>
2015-09-08 15:45:43 -0700

    Demonstrate OnError not working in Release mode

--- src/Nancy.Demo.Razor.Localization/DemoBootstrapper.cs
+++ src/Nancy.Demo.Razor.Localization/DemoBootstrapper.cs
@@ -1,6 +1,9 @@
 ﻿namespace Nancy.Demo.Razor.Localization
 {
     using Bootstrapper;
+    using Nancy.ErrorHandling;
+    using Nancy.Responses;
+    using Nancy.TinyIoc;
 
     public class DemoBootstrapper : DefaultNancyBootstrapper
     {
@@ -10,6 +13,29 @@
             {
                 return NancyInternalConfiguration.WithOverrides(x => x.ResourceAssemblyProvider = typeof(CustomResourceAssemblyProvider));
             }
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            pipelines.OnError += (ctx, ex) =>
+            {
+                ctx.Items["ON_ERROR"] = "value from OnError";
+                return null;
+            };
+        }
+    }
+
+    public class StatusCodeHandler : IStatusCodeHandler
+    {
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return true;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            context.Response = new TextResponse(statusCode, (string)context.Items["ON_ERROR"]);
         }
     }
 }
